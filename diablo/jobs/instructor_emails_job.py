82641cbf664b06f777eb17a151837788e0d8240b
Paul Kerschen <kerschen@berkeley.edu>
2020-05-12 13:56:36 -0700

    DIABLO-262 Send invites from instructor emails job

--- diablo/jobs/instructor_emails_job.py
+++ diablo/jobs/instructor_emails_job.py
@@ -22,58 +22,80 @@ SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED
 "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
 ENHANCEMENTS, OR MODIFICATIONS.
 """
+
 from diablo.externals.b_connected import BConnected
 from diablo.jobs.background_job_manager import BackgroundJobError
 from diablo.jobs.base_job import BaseJob
-from diablo.merged.emailer import interpolate_email_content, send_system_error_email
+from diablo.merged.emailer import interpolate_email_content, send_course_related_email, send_system_error_email
 from diablo.models.email_template import EmailTemplate
 from diablo.models.scheduled import Scheduled
 from diablo.models.sis_section import SisSection
 from flask import current_app as app
 
 
-class InstructorsEmailsJob(BaseJob):
+class InstructorEmailsJob(BaseJob):
 
     def run(self):
-        term_id = app.config['CURRENT_TERM_ID']
-        all_scheduled = Scheduled.get_all_scheduled(term_id=term_id)
-        if all_scheduled:
-            courses = SisSection.get_courses(term_id=term_id, section_ids=[s.section_id for s in all_scheduled])
-            courses_per_section_id = dict((course['sectionId'], course) for course in courses)
-            for scheduled in all_scheduled:
-                course = courses_per_section_id[scheduled.section_id]
-                if course:
-                    if scheduled.room_id != course['room']['id']:
-                        template_type = 'room_change_no_longer_eligible'
-                        email_template = EmailTemplate.get_template_by_type(template_type)
-                        if email_template:
-                            for instructor in course['instructor']:
-                                BConnected().send(
-                                    message=interpolate_email_content(
-                                        templated_string=email_template.message,
-                                        course=course,
-                                        instructor_name=instructor['name'],
-                                        recipient_name=instructor['name'],
-                                        recording_type_name=scheduled.recording_type,
-                                    ),
-                                    recipients=course['instructors'],
-                                    subject_line=interpolate_email_content(
-                                        templated_string=email_template.subject_line,
-                                        course=course,
-                                        instructor_name=instructor['name'],
-                                        recipient_name=instructor['name'],
-                                        recording_type_name=scheduled.recording_type,
-                                    ),
-                                )
-                        else:
-                            raise BackgroundJobError(f"""
-                                No email template of type {template_type} is available.
-                                {course['label']} instructors were NOT notified of scheduled: {scheduled}.
-                            """)
-                else:
-                    error = f'section_id of scheduled recordings was not found in SIS data: {scheduled}'
-                    app.logger.error(error)
-                    send_system_error_email(message=error)
+        self.term_id = app.config['CURRENT_TERM_ID']
+        self.email_new_invites()
+        self.email_scheduled_courses()
+
+    def email_new_invites(self):
+        for course in SisSection.get_courses(term_id=self.term_id):
+            if course['hasOptedOut']:
+                next
+            new_instructors = [i for i in course['instructors'] if not i['wasSentInvite']]
+            if new_instructors:
+                if not send_course_related_email(
+                    course=course,
+                    recipients=new_instructors,
+                    template_type='invitation',
+                    term_id=self.term_id,
+                ):
+                    app.logger.error(f"""
+                        Failed to invite {len(new_instructors)} new instructors to section ID {course['sectionId']}).
+                        Instructors: {new_instructors}""")
+
+    def email_scheduled_courses(self):
+        all_scheduled = Scheduled.get_all_scheduled(term_id=self.term_id)
+        if not all_scheduled:
+            return
+        courses = SisSection.get_courses(term_id=self.term_id, section_ids=[s.section_id for s in all_scheduled])
+        courses_per_section_id = dict((course['sectionId'], course) for course in courses)
+        for scheduled in all_scheduled:
+            course = courses_per_section_id[scheduled.section_id]
+            if course:
+                if scheduled.room_id != course['room']['id']:
+                    template_type = 'room_change_no_longer_eligible'
+                    email_template = EmailTemplate.get_template_by_type(template_type)
+                    if email_template:
+                        for instructor in course['instructors']:
+                            BConnected().send(
+                                message=interpolate_email_content(
+                                    templated_string=email_template.message,
+                                    course=course,
+                                    instructor_name=instructor['name'],
+                                    recipient_name=instructor['name'],
+                                    recording_type_name=scheduled.recording_type,
+                                ),
+                                recipients=course['instructors'],
+                                subject_line=interpolate_email_content(
+                                    templated_string=email_template.subject_line,
+                                    course=course,
+                                    instructor_name=instructor['name'],
+                                    recipient_name=instructor['name'],
+                                    recording_type_name=scheduled.recording_type,
+                                ),
+                            )
+                    else:
+                        raise BackgroundJobError(f"""
+                            No email template of type {template_type} is available.
+                            {course['label']} instructors were NOT notified of scheduled: {scheduled}.
+                        """)
+            else:
+                error = f'section_id of scheduled recordings was not found in SIS data: {scheduled}'
+                app.logger.error(error)
+                send_system_error_email(message=error)
 
     @classmethod
     def description(cls):
