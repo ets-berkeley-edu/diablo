e82dedd713869a9fb0c98b9d8f7419ee9d54f87a
John Crossman <johncrossman@berkeley.edu>
2020-06-19 11:24:21 -0700

    DIABLO-432, prep db_utils to load mock SIS data with hybrid location scenarios

--- tests/test_lib/test_interpolator.py
+++ tests/test_lib/test_interpolator.py
@@ -66,10 +66,10 @@ def _get_mock_recipient():
 def _get_expected_email():
     return """Hello William Peter Blatty,
 
-    Your Spring 2020 course COMPSCI 61B, "Data Structures", is eligible for Course Capture.
+    Your Fall 2020 course COMPSCI 61B, "Data Structures", is eligible for Course Capture.
     This course meets Monday, Wednesday and Friday from 3:00 pm to 3:59 pm.
 
-    Go to <a href="http://foo.berkeley.edu">https://manage-test.coursecapture.berkeley.edu/course/2202/50001</a> and sign up.
+    Go to <a href="http://foo.berkeley.edu">https://manage-test.coursecapture.berkeley.edu/course/2208/50001</a> and sign up.
 
     Thank you."""
 
