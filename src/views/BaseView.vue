94bea5aff42477fca78b55441a73cc1e07b487d0
John Crossman <johncrossman@berkeley.edu>
2020-06-19 11:03:59 -0700

    DIABLO-323, fix herky-jerky side nav

--- src/views/BaseView.vue
+++ src/views/BaseView.vue
@@ -16,9 +16,9 @@
             <v-icon color="icon-nav-default">{{ $currentUser.isAdmin ? 'mdi-auto-fix' : 'mdi-home' }}</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-content>
+            <v-list-item-title>
               {{ $currentUser.isAdmin ? 'Ouija Board' : 'Home' }}
-            </v-list-item-content>
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item
