a1ca4d74da5fdcaafbbe34818ba58bdef24011a0
Paul Kerschen <kerschen@berkeley.edu>
2020-07-02 11:24:26 -0700

    DIABLO-485 Exclude non-room locations from xlist calculation

--- diablo/jobs/util.py
+++ diablo/jobs/util.py
@@ -98,7 +98,8 @@ def refresh_cross_listings(term_id):
                     AND meeting_days <> ''
                     AND meeting_end_date IS NOT NULL
                     AND meeting_end_time <> ''
-                    AND meeting_location <> ''
+                    AND meeting_location IS NOT NULL
+                    AND meeting_location NOT IN ('', 'Internet/Online', 'Off Campus', 'Requested General Assignment')
                     AND meeting_start_date IS NOT NULL
                     AND meeting_start_time <> ''
                 ORDER BY schedule, section_id
