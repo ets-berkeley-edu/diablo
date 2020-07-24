44590c738ea147fa0af6782564694398f05a1181
John Crossman <johncrossman@berkeley.edu>
2020-07-24 09:46:52 -0700

    DIABLO-531, sr-only on day names; sr-skip on 'moe' and 'wee'

--- src/components/util/Days.vue
+++ src/components/util/Days.vue
@@ -2,7 +2,8 @@
   <span>
     <span class="sr-only">Days of the week:</span>
     <span v-for="(day, index) in namesOfDays" :key="index">
-      <abbr :title="day">{{ day.slice(0, 2).toUpperCase() }}</abbr><span v-if="showComma(namesOfDays, index)" :class="{'pr-1': omitComma}"><span :class="{'sr-only': omitComma}">, </span></span>
+      <span class="sr-only">{{ day }}</span>
+      <span class="sr-skip" :title="day">{{ day.slice(0, 2).toUpperCase() }}</span><span v-if="showComma(namesOfDays, index)" :class="{'pr-1': omitComma}"><span :class="{'sr-only': omitComma}">, </span></span>
     </span>
   </span>
 </template>
