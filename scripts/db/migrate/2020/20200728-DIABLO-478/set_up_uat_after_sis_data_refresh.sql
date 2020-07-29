1b7e60b66950d872c32c4e7d083f210e91c7f78e
John Crossman <johncrossman@berkeley.edu>
2020-07-28 17:08:27 -0700

    DIABLO-478, SQL script to run prior to three day UAT exercise

---
+++ scripts/db/migrate/2020/20200728-DIABLO-478/set_up_uat_after_sis_data_refresh.sql
@@ -0,0 +1,42 @@
+/**
+ * Copyright ©2020. The Regents of the University of California (Regents). All Rights Reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software and its documentation
+ * for educational, research, and not-for-profit purposes, without fee and without a
+ * signed licensing agreement, is hereby granted, provided that the above copyright
+ * notice, this paragraph and the following two paragraphs appear in all copies,
+ * modifications, and distributions.
+ *
+ * Contact The Office of Technology Licensing, UC Berkeley, 2150 Shattuck Avenue,
+ * Suite 510, Berkeley, CA 94720-1620, (510) 643-7201, otl@berkeley.edu,
+ * http://ipira.berkeley.edu/industry-info for commercial licensing opportunities.
+ *
+ * IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+ * INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+ * THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS BEEN ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ * SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED
+ * "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ * ENHANCEMENTS, OR MODIFICATIONS.
+ */
+
+BEGIN;
+
+DELETE FROM approvals;
+DELETE FROM course_preferences;
+DELETE FROM scheduled;
+DELETE FROM queued_emails;
+DELETE FROM sent_emails;
+DELETE FROM canvas_course_sites;
+DELETE FROM cross_listings;
+DELETE FROM sis_sections WHERE section_id < 1000000000;
+DELETE FROM instructors WHERE uid NOT IN (:'u1', :'u2', :'u3', :'u4', :'u5', :'u6', :'u7');
+
+UPDATE rooms SET capability = NULL WHERE location != 'Cory 277';
+
+-- TODO: Canvas course sites for courses above
+
+COMMIT;
