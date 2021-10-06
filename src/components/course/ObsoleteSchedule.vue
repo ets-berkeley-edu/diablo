0f5e172d8fb484e7bb3548292b4fbde8a817e783
John Crossman <johncrossman@berkeley.edu>
2021-10-06 16:48:57 -0700

    DIABLO-679, UI tidying on /changes page for cancelled sections

--- src/components/course/ObsoleteSchedule.vue
+++ src/components/course/ObsoleteSchedule.vue
@@ -6,10 +6,10 @@
           :id="`link-course-${course.sectionId}`"
           :to="`/course/${$config.currentTermId}/${course.sectionId}`"
         >
-          <h2>{{ course.label }}</h2>
+          <h2 :class="{'line-through': course.deletedAt}">{{ course.label }}</h2>
         </router-link>
-        <div class="pt-2">
-          <h3 id="course-title">{{ course.courseTitle }}</h3>
+        <div class="pr-2">
+          <h3 id="course-title" :class="{'line-through': course.deletedAt}">{{ course.courseTitle }}</h3>
           Section ID: {{ course.sectionId }}
         </div>
       </v-card>
@@ -24,7 +24,7 @@
             {{ summary }}
           </v-card-subtitle>
           <v-card-text>
-            <div v-if="instructorsNew.length">
+            <div v-if="instructorsNew.length && !course.deletedAt">
               <h5>Joined course <i>after</i> recordings were scheduled</h5>
               <div v-for="instructor in instructorsNew" :key="instructor.uid">
                 <Instructor
@@ -34,7 +34,17 @@
                 />
               </div>
             </div>
-            <div v-if="instructorsObsolete.length" :class="{'pt-2': instructorsNew.length}">
+            <div v-if="course.deletedAt" class="pb-2">
+              <a
+                id="link-to-edit-kaltura-event"
+                :href="`${$config.kalturaMediaSpaceUrl}/recscheduling/index/edit-event/eventid/${course.scheduled.kalturaScheduleId}`"
+                target="_blank"
+                aria-label="Open Kaltura MediaSpace in a new window"
+              >
+                Kaltura series {{ course.scheduled.kalturaScheduleId }} <v-icon small class="pb-1">mdi-open-in-new</v-icon>
+              </a>
+            </div>
+            <div v-if="instructorsObsolete.length && !course.deletedAt" :class="{'pt-2': instructorsNew.length}">
               <h5>Obsolete Instructors</h5>
               <div v-for="instructor in instructorsObsolete" :key="instructor.uid">
                 <Instructor
@@ -45,7 +55,7 @@
               </div>
             </div>
             <div
-              v-if="isRoomObsolete || course.scheduled.hasObsoleteDates || course.scheduled.hasObsoleteTimes"
+              v-if="(isRoomObsolete || course.scheduled.hasObsoleteDates || course.scheduled.hasObsoleteTimes) && !course.deletedAt"
               :class="{'pt-2': course.scheduled.hasObsoleteInstructors}"
             >
               <h5>Meeting</h5>
@@ -59,15 +69,8 @@
                     :id="`tooltip-course-${course.sectionId}-obsolete-dates`"
                     bottom
                   >
-                    <template #activator="{on, attrs}">
-                      <v-icon
-                        class="pr-1"
-                        color="yellow darken-2"
-                        v-bind="attrs"
-                        v-on="on"
-                      >
-                        mdi-calendar-export
-                      </v-icon>
+                    <template>
+                      <v-icon class="pr-1" color="yellow darken-2">mdi-calendar-export</v-icon>
                     </template>
                     Meeting dates have changed.
                   </v-tooltip>
@@ -81,15 +84,8 @@
                     :id="`tooltip-course-${course.sectionId}-obsolete-times`"
                     bottom
                   >
-                    <template #activator="{on, attrs}">
-                      <v-icon
-                        class="pr-1"
-                        color="yellow darken-2"
-                        v-bind="attrs"
-                        v-on="on"
-                      >
-                        mdi-clock-alert-outline
-                      </v-icon>
+                    <template>
+                      <v-icon class="pr-1" color="yellow darken-2">mdi-clock-alert-outline</v-icon>
                     </template>
                     Meeting times have changed.
                   </v-tooltip>
@@ -112,7 +108,11 @@
             SIS Data last imported on {{ sisDataImportDate.finishedAt | moment('MMM D, YYYY') }}.
           </v-card-subtitle>
           <v-card-text>
-            <div v-if="course.scheduled.hasObsoleteInstructors">
+            <div v-if="course.deletedAt">
+              <v-icon color="red" class="font-weight-bold pb-1 pl-0">mdi-close</v-icon>
+              <span class="font-weight-bold red--text">UC Berkeley has canceled this section.</span>
+            </div>
+            <div v-if="course.scheduled.hasObsoleteInstructors && !course.deletedAt">
               <h5>Instructors</h5>
               <div v-if="course.instructors.length">
                 <div v-for="instructor in course.instructors" :key="instructor.uid">
@@ -124,7 +124,7 @@
               </div>
             </div>
             <div
-              v-if="isRoomObsolete || course.scheduled.hasObsoleteDates || course.scheduled.hasObsoleteTimes"
+              v-if="(isRoomObsolete || course.scheduled.hasObsoleteDates || course.scheduled.hasObsoleteTimes) && !course.deletedAt"
               :class="{'pt-2': course.scheduled.hasObsoleteInstructors}"
             >
               <h5>All Meetings</h5>
@@ -193,10 +193,12 @@ export default {
         times: this.course.scheduled.hasObsoleteTimes
       }}
     }
-    const obsoleteItems = this.$_.keys(this.$_.pickBy(considerations))
-    this.summary = this.$_.capitalize(`${this.oxfordJoin(obsoleteItems)} ${hasObsoleteRoom && obsoleteItems.length === 1 ? 'is' : 'are'} obsolete.`)
-    this.meetings = this.course.meetings.eligible.concat(this.course.meetings.ineligible)
-    this.isRoomObsolete = !this.isInRoom(this.course, this.course.scheduled.room)
+    if (!this.course.deletedAt) {
+      const obsoleteItems = this.$_.keys(this.$_.pickBy(considerations))
+      this.summary = this.$_.capitalize(`${this.oxfordJoin(obsoleteItems)} ${hasObsoleteRoom && obsoleteItems.length === 1 ? 'is' : 'are'} obsolete.`)
+      this.meetings = this.course.meetings.eligible.concat(this.course.meetings.ineligible)
+      this.isRoomObsolete = !this.isInRoom(this.course, this.course.scheduled.room)
+    }
     getLastSuccessfulRun('sis_data_refresh').then(data => {
       this.sisDataImportDate = data
     })
