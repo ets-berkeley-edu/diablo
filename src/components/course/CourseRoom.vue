b9555637cb92fa2c88d4f4089b5b7791e64727eb
John Crossman <johncrossman@berkeley.edu>
2022-09-18 21:12:47 -0700

    DIABLO-715, instructor_emails_job alert for scheduled recordings of course w/o meeting location

--- src/components/course/CourseRoom.vue
+++ src/components/course/CourseRoom.vue
@@ -2,24 +2,25 @@
   <div class="d-flex">
     <v-tooltip
       v-if="!hideEligibility"
-      :id="`tooltip-course-${course.sectionId}-room-${room.id}-ineligible`"
+      :id="`tooltip-course-${course.sectionId}-room-${roomId}-ineligible`"
       bottom
     >
       <template #activator="{on, attrs}">
         <v-icon
           class="pr-1"
-          :color="room.capability ? 'light-green' : 'yellow darken-2'"
+          :color="capability ? 'light-green' : 'yellow darken-2'"
           v-bind="attrs"
           v-on="on"
         >
-          {{ room.capability ? 'mdi-video-plus' : 'mdi-video-off-outline' }}
+          {{ capability ? 'mdi-video-plus' : 'mdi-video-off-outline' }}
         </v-icon>
       </template>
-      {{ room.location }} is {{ room.capability ? '' : 'not' }} capture-enabled.
+      <span v-if="location">{{ location }} is {{ capability ? '' : 'not' }} capture-enabled.</span>
+      <span v-if="!location">No meeting location.</span>
     </v-tooltip>
     <v-tooltip
       v-if="!isInRoom(course, room)"
-      :id="`tooltip-course-${course.sectionId}-room-${room.id}-obsolete`"
+      :id="`tooltip-course-${course.sectionId}-room-${roomId}-obsolete`"
       bottom
     >
       <template #activator="{on, attrs}">
@@ -32,14 +33,15 @@
           mdi-map-marker-remove-outline
         </v-icon>
       </template>
-      This course moved out of {{ room.location }}.
+      <span v-if="location">This course moved out of {{ location }}.</span>
+      <span v-if="!location">No meeting location.</span>
     </v-tooltip>
-    <div>
+    <div v-if="roomId">
       <router-link
-        :id="`course-${course.sectionId}-room-${room.id}`"
-        :to="`/room/${room.id}`"
+        :id="`course-${course.sectionId}-room-${roomId}`"
+        :to="`/room/${roomId}`"
       >
-        {{ room.location }}
+        {{ location }}
       </router-link>
     </div>
   </div>
@@ -61,8 +63,18 @@ export default {
     },
     room: {
       required: true,
-      type: Object
+      validator: prop => prop || prop === null
     }
+  },
+  data: () => ({
+    capability: undefined,
+    location: undefined,
+    roomId: undefined
+  }),
+  created() {
+    this.capability = this.$_.get(this.room, 'capability')
+    this.location = this.$_.get(this.room, 'location')
+    this.roomId = this.$_.get(this.room, 'id')
   }
 }
 </script>
