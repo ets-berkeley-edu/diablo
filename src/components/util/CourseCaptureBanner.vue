24304a756ca949d365a192e16b402e26ac7b4ab4
John Crossman <johncrossman@berkeley.edu>
2022-12-15 12:17:05 -0800

    DIABLO-780, minor py and node upgrades; animated h1 intro w/ term info

---
+++ src/components/util/CourseCaptureBanner.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="banner">
+    <transition><div v-if="showPrefix">This is</div></transition>
+    <b>
+      <div class="banner-inner">
+        Course Capture<br />
+        {{ $config.currentTermName }}<br />
+        Bear Territory
+      </div>
+    </b>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CourseCaptureBanner',
+  data: () => ({
+    showPrefix: true
+  }),
+  created() {
+    setTimeout(() => {
+      this.showPrefix = false
+    }, 8000)
+  }
+}
+</script>
+
+<style scoped>
+.banner {
+  font: normal 40px/50px Montserrat, sans-serif;
+  color: #999;
+}
+.banner div {
+  height: 50px;
+  float: left;
+  margin-right: 0.3em;
+}
+.banner b {
+  float: left;
+  overflow: hidden;
+  position: relative;
+  height: 50px;
+  top: 0px;
+}
+.banner .banner-inner {
+  display: inline-block;
+  color: #e74c3c;
+  position: relative;
+  white-space: nowrap;
+  top: 0;
+  left: 0;
+
+  /*animation*/
+  -webkit-animation:move 5s;
+     -moz-animation:move 5s;
+      -ms-animation:move 5s;
+       -o-animation:move 5s;
+          animation:move 5s;
+  /*animation-iteration-count*/
+  -webkit-animation-iteration-count: 1;
+     -moz-animation-iteration-count: 1;
+      -ms-animation-iteration-count: 1;
+       -o-animation-iteration-count: 1;
+          animation-iteration-count: 1;
+  /*animation-delay*/
+  -webkit-animation-delay: 2s;
+     -moz-animation-delay: 2s;
+      -ms-animation-delay: 2s;
+       -o-animation-delay: 2s;
+          animation-delay: 2s;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 1.0s ease-out;
+}
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+@keyframes move {
+  0%  { top: 0px; }
+  20% { top: -50px; }
+  40% { top: -100px; }
+}
+@-webkit-keyframes move {
+  0%  { top: 0px; }
+  20% { top: -50px; }
+  40% { top: -100px; }
+}
+@-moz-keyframes move {
+  0%  { top: 0px; }
+  20% { top: -50px; }
+  40% { top: -100px; }
+}
+@-o-keyframes move {
+  0%  { top: 0px; }
+  20% { top: -50px; }
+  40% { top: -100px; }
+}
+@keyframes move {
+  0%  { top: 0px; }
+  20% { top: -50px; }
+  40% { top: -100px; }
+}
+</style>
