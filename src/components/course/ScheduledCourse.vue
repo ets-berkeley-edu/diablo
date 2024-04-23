0d3f9e95973750a7b76fa9e12bae2fdfbc1bdfdf
Pauline Kerschen <kerschen@berkeley.edu>
2024-04-23 16:49:04 -0700

    DIABLO-860 Display multiple schedulings per course in front end

--- src/components/course/ScheduledCourse.vue
+++ src/components/course/ScheduledCourse.vue
@@ -1,92 +1,94 @@
 <template>
-  <v-list>
-    <v-list-item-title class="pl-4 pt-3">
-      <h4 class="title">Recordings scheduled</h4>
-      <div v-if="$currentUser.isAdmin" class="d-flex align-bottom">
-        <div>
-          <a
-            id="link-to-edit-kaltura-event"
-            :href="`${$config.kalturaMediaSpaceUrl}/recscheduling/index/edit-event/eventid/${course.scheduled.kalturaScheduleId}`"
-            target="_blank"
-            aria-label="Open Kaltura MediaSpace in a new window"
-          >
-            Kaltura series {{ course.scheduled.kalturaScheduleId }} <v-icon small class="pb-1">mdi-open-in-new</v-icon>
-          </a>
-        </div>
-      </div>
-    </v-list-item-title>
-    <v-list-item two-line>
-      <v-list-item-content v-if="course.scheduled.hasObsoleteRoom">
-        <v-list-item-title>
-          <CourseRoom :course="course" :hide-eligibility="true" :room="course.scheduled.room" />
-        </v-list-item-title>
-      </v-list-item-content>
-      <v-list-item-content>
-        <v-list-item-title>Scheduled on</v-list-item-title>
-        <v-list-item-subtitle>{{ course.scheduled.createdAt | moment('MMM DD, YYYY') }}</v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-content>
-        <v-list-item-title>Recording Type</v-list-item-title>
-        <v-list-item-subtitle>{{ course.scheduled.recordingTypeName }}</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-    <v-list-item two-line>
-      <v-list-item-content>
-        <v-list-item-title>Publish Type</v-list-item-title>
-        <v-list-item-subtitle>{{ course.scheduled.publishTypeName }}</v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-content>
-        <v-list-item-title>Admin Proxy Publish</v-list-item-title>
-        <v-list-item-subtitle id="admin-proxy-status">
-          {{ course.canAprxInstructorsEditRecordings ? 'Yes' : 'No' }}
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-    <v-list-item v-if="$currentUser.isAdmin && adminAlerts.length" two-line class="pb-3">
-      <v-list-item-content>
-        <v-list-item-title>
-          <v-icon class="pb-1 pr-1" color="red">mdi-alert</v-icon>
-          {{ $_.capitalize(oxfordJoin(adminAlerts).toLowerCase()) }}
-        </v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-    <v-card-text v-if="currentUserMustApprove">
-      <v-container>
-        <v-row class="pb-4">
-          <h4 class="ml-3 title">We need your approval</h4>
-        </v-row>
-        <v-row no-gutters align="start">
-          <v-col md="auto">
-            <v-checkbox id="agree-to-terms-checkbox" v-model="agreedToTerms" class="mt-0" />
-          </v-col>
-          <v-col>
-            <TermsAgreementText />
-          </v-col>
-        </v-row>
-        <v-row lg="2">
-          <v-col>
-            <v-btn
-              id="btn-approve"
-              class="float-right"
-              color="success"
-              :disabled="!agreedToTerms || isApproving"
-              @click="approve"
+  <div>
+    <h4 class="title">Recordings scheduled</h4>
+    <v-list v-for="scheduled in course.scheduled" :key="scheduled.kalturaScheduleId">
+      <v-list-item-title class="pl-4 pt-3">
+        <div v-if="$currentUser.isAdmin" class="d-flex align-bottom">
+          <div>
+            <a
+              id="link-to-edit-kaltura-event"
+              :href="`${$config.kalturaMediaSpaceUrl}/recscheduling/index/edit-event/eventid/${scheduled.kalturaScheduleId}`"
+              target="_blank"
+              aria-label="Open Kaltura MediaSpace in a new window"
             >
-              <v-progress-circular
-                v-if="isApproving"
-                class="mr-2"
-                color="primary"
-                indeterminate
-                size="18"
-                width="3"
-              ></v-progress-circular>
-              {{ isApproving ? 'Approving' : 'Approve' }}
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card-text>
-  </v-list>
+              Kaltura series {{ course.scheduled.kalturaScheduleId }} <v-icon small class="pb-1">mdi-open-in-new</v-icon>
+            </a>
+          </div>
+        </div>
+      </v-list-item-title>
+      <v-list-item two-line>
+        <v-list-item-content v-if="scheduled.hasObsoleteRoom">
+          <v-list-item-title>
+            <CourseRoom :course="course" :hide-eligibility="true" :room="scheduled.room" />
+          </v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-content>
+          <v-list-item-title>Scheduled on</v-list-item-title>
+          <v-list-item-subtitle>{{ scheduled.createdAt | moment('MMM DD, YYYY') }}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-content>
+          <v-list-item-title>Recording Type</v-list-item-title>
+          <v-list-item-subtitle>{{ scheduled.recordingTypeName }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>Publish Type</v-list-item-title>
+          <v-list-item-subtitle>{{ scheduled.publishTypeName }}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-content>
+          <v-list-item-title>Admin Proxy Publish</v-list-item-title>
+          <v-list-item-subtitle id="admin-proxy-status">
+            {{ course.canAprxInstructorsEditRecordings ? 'Yes' : 'No' }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item v-if="$currentUser.isAdmin && adminAlerts.length" two-line class="pb-3">
+        <v-list-item-content>
+          <v-list-item-title>
+            <v-icon class="pb-1 pr-1" color="red">mdi-alert</v-icon>
+            {{ $_.capitalize(oxfordJoin(adminAlerts).toLowerCase()) }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-card-text v-if="currentUserMustApprove">
+        <v-container>
+          <v-row class="pb-4">
+            <h4 class="ml-3 title">We need your approval</h4>
+          </v-row>
+          <v-row no-gutters align="start">
+            <v-col md="auto">
+              <v-checkbox id="agree-to-terms-checkbox" v-model="agreedToTerms" class="mt-0" />
+            </v-col>
+            <v-col>
+              <TermsAgreementText />
+            </v-col>
+          </v-row>
+          <v-row lg="2">
+            <v-col>
+              <v-btn
+                id="btn-approve"
+                class="float-right"
+                color="success"
+                :disabled="!agreedToTerms || isApproving"
+                @click="approve"
+              >
+                <v-progress-circular
+                  v-if="isApproving"
+                  class="mr-2"
+                  color="primary"
+                  indeterminate
+                  size="18"
+                  width="3"
+                ></v-progress-circular>
+                {{ isApproving ? 'Approving' : 'Approve' }}
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+    </v-list>
+  </div>
 </template>
 
 <script>
