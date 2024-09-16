73781aa4fddd3ce8df4a7e3986cd980c0bfb70bb
Pauline Kerschen <kerschen@berkeley.edu>
2024-09-16 13:56:09 -0700

    DIABLO-1001 Display ineligible courses to instructors as well

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,88 +28,95 @@
         />
       </v-row>
       <Spinner v-if="refreshingCourses" />
-      <v-data-table
-        v-if="!refreshingCourses && courses.length"
-        disable-pagination
-        disable-sort
-        :headers="headers"
-        :hide-default-footer="true"
-        :items="courses"
-        :items-per-page="100"
-        class="elevation-1"
-      >
-        <template #body="{items}">
-          <tbody>
-            <template v-for="course in items" :id="`course-${course.sectionId}`">
-              <tr :key="course.sectionId">
-                <td class="text-no-wrap" :class="{'pt-3 pb-3': course.courseCodes.length > 1, 'border-bottom-zero': course.meetings.eligible.length > 1}">
-                  <div v-for="(courseCode, index) in course.courseCodes" :key="courseCode">
-                    <router-link
-                      v-if="index === 0"
-                      :id="`link-course-${course.sectionId}`"
-                      :to="`/course/${$config.currentTermId}/${course.sectionId}`"
-                    >
-                      {{ courseCode }}
-                    </router-link>
-                    <span v-if="index > 0">{{ courseCode }}</span>
-                  </div>
-                </td>
-                <td :class="{'border-bottom-zero': course.meetings.eligible.length > 1}">
-                  {{ course.courseTitle || '&mdash;' }}
-                </td>
-                <td :class="{'border-bottom-zero': course.meetings.eligible.length > 1}">
-                  {{ oxfordJoin($_.map(course.instructors, 'name')) }}
-                </td>
-                <td :class="{'border-bottom-zero': course.meetings.eligible.length > 1}" class="text-no-wrap">
-                  {{ course.meeting.room.location }}
-                </td>
-                <td :class="{'border-bottom-zero': course.meetings.eligible.length > 1}" class="text-no-wrap">
-                  <Days v-if="course.meeting.daysNames.length" :names-of-days="course.meeting.daysNames" />
-                  <span v-if="!course.meeting.daysNames.length">&mdash;</span>
-                </td>
-                <td :class="{'border-bottom-zero': course.meetings.eligible.length > 1}" class="text-no-wrap">
-                  <div v-if="course.nonstandardMeetingDates" class="pt-2">
-                    <span class="text-no-wrap">{{ course.meeting.startDate | moment('MMM D, YYYY') }} - </span>
-                    <span class="text-no-wrap">{{ course.meeting.endDate | moment('MMM D, YYYY') }}</span>
-                  </div>
-                  {{ course.meeting.startTimeFormatted }} - {{ course.meeting.endTimeFormatted }}
-                </td>
-                <td :class="{'border-bottom-zero': course.meetings.eligible.length > 1}" class="text-no-wrap">
-                  <ToggleOptOut
-                    :term-id="`${course.termId}`"
-                    :section-id="`${course.sectionId}`"
-                    :instructor-uid="$currentUser.uid"
-                    :initial-value="course.hasOptedOut"
-                    :disabled="course.hasBlanketOptedOut"
-                  />
-                </td>
-              </tr>
-              <tr v-for="index in course.meetings.eligible.length - 1" :key="index">
-                <td></td>
-                <td></td>
-                <td></td>
-                <td class="pt-0 text-no-wrap">
-                  {{ course.meetings.eligible[index].room.location }}
-                </td>
-                <td class="text-no-wrap">
-                  <Days :names-of-days="course.meetings.eligible[index].daysNames" />
-                </td>
-                <td class="text-no-wrap">
-                  <div v-if="course.nonstandardMeetingDates" class="pt-2">
-                    <span class="text-no-wrap">{{ course.meetings.eligible[index].startDate | moment('MMM D, YYYY') }} - </span>
-                    <span class="text-no-wrap">{{ course.meetings.eligible[index].endDate | moment('MMM D, YYYY') }}</span>
-                  </div>
-                  <div :class="{'pb-2': course.nonstandardMeetingDates && index === course.meetings.eligible.length - 1}">
-                    {{ course.meetings.eligible[index].startTimeFormatted }} - {{ course.meetings.eligible[index].endTimeFormatted }}
-                  </div>
-                </td>
-                <td></td>
-              </tr>
+      <template v-for="(courses, index) in [eligibleCourses, ineligibleCourses]">
+        <v-row v-if="!refreshingCourses && courses.length" :key="index">
+          <h2 class="px-4">
+            {{ index === 0 ? 'Courses eligible for capture' : 'Courses not in a course capture classroom' }}
+          </h2>
+          <v-data-table
+            :id="index === 0 ? 'courses-table-eligible' : 'courses-table-ineligible'"
+            disable-pagination
+            disable-sort
+            :headers="index === 0 ? eligibleHeaders : ineligibleHeaders"
+            :hide-default-footer="true"
+            :items="courses"
+            :items-per-page="100"
+            class="elevation-1 w-100 ma-4"
+          >
+            <template #body="{items}">
+              <tbody>
+                <template v-for="course in items" :id="`course-${course.sectionId}`">
+                  <tr :key="course.sectionId">
+                    <td class="text-no-wrap" :class="{'pt-3 pb-3': course.courseCodes.length > 1, 'border-bottom-zero': course.displayMeetings.length > 1}">
+                      <div v-for="(courseCode, courseCodeIndex) in course.courseCodes" :key="courseCode">
+                        <router-link
+                          v-if="courseCodeIndex === 0"
+                          :id="`link-course-${course.sectionId}`"
+                          :to="`/course/${$config.currentTermId}/${course.sectionId}`"
+                        >
+                          {{ courseCode }}
+                        </router-link>
+                        <span v-if="courseCodeIndex > 0">{{ courseCode }}</span>
+                      </div>
+                    </td>
+                    <td :class="{'border-bottom-zero': course.displayMeetings.length > 1}">
+                      {{ course.courseTitle || '&mdash;' }}
+                    </td>
+                    <td :class="{'border-bottom-zero': course.displayMeetings.length > 1}">
+                      {{ oxfordJoin($_.map(course.instructors, 'name')) }}
+                    </td>
+                    <td :class="{'border-bottom-zero': course.displayMeetings.length > 1}" class="text-no-wrap">
+                      {{ course.displayMeetings[0].room.location }}
+                    </td>
+                    <td :class="{'border-bottom-zero': course.displayMeetings.length > 1}" class="text-no-wrap">
+                      <Days v-if="course.displayMeetings[0].daysNames.length" :names-of-days="course.displayMeetings[0].daysNames" />
+                      <span v-if="!course.displayMeetings[0].daysNames.length">&mdash;</span>
+                    </td>
+                    <td :class="{'border-bottom-zero': course.displayMeetings.length > 1}" class="text-no-wrap">
+                      <div v-if="course.nonstandardMeetingDates" class="pt-2">
+                        <span class="text-no-wrap">{{ course.displayMeetings[0].startDate | moment('MMM D, YYYY') }} - </span>
+                        <span class="text-no-wrap">{{ course.displayMeetings[0].endDate | moment('MMM D, YYYY') }}</span>
+                      </div>
+                      {{ course.displayMeetings[0].startTimeFormatted }} - {{ course.displayMeetings[0].endTimeFormatted }}
+                    </td>
+                    <td v-if="index === 0" :class="{'border-bottom-zero': course.displayMeetings[0].eligible.length > 1}" class="text-no-wrap">
+                      <ToggleOptOut
+                        :term-id="`${course.termId}`"
+                        :section-id="`${course.sectionId}`"
+                        :instructor-uid="$currentUser.uid"
+                        :initial-value="course.hasOptedOut"
+                        :disabled="course.hasBlanketOptedOut"
+                      />
+                    </td>
+                  </tr>
+                  <tr v-for="meetingIndex in course.displayMeetings.length - 1" :key="meetingIndex">
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td class="pt-0 text-no-wrap">
+                      {{ course.displayMeetings[meetingIndex].room.location }}
+                    </td>
+                    <td class="text-no-wrap">
+                      <Days :names-of-days="course.displayMeetings[meetingIndex].daysNames" />
+                    </td>
+                    <td class="text-no-wrap">
+                      <div v-if="course.nonstandardMeetingDates" class="pt-2">
+                        <span class="text-no-wrap">{{ course.displayMeetings[meetingIndex].startDate | moment('MMM D, YYYY') }} - </span>
+                        <span class="text-no-wrap">{{ course.displayMeetings[meetingIndex].endDate | moment('MMM D, YYYY') }}</span>
+                      </div>
+                      <div :class="{'pb-2': course.nonstandardMeetingDates && meetingIndex === course.displayMeetings.length - 1}">
+                        {{ course.displayMeetings[meetingIndex].startTimeFormatted }} - {{ course.displayMeetings[meetingIndex].endTimeFormatted }}
+                      </div>
+                    </td>
+                    <td></td>
+                  </tr>
+                </template>
+              </tbody>
             </template>
-          </tbody>
-        </template>
-      </v-data-table>
-      <v-row v-if="!courses.length" class="ma-4 text-no-wrap title">
+          </v-data-table>
+        </v-row>
+      </template>
+      <v-row v-if="!eligibleCourses.length && !ineligibleCourses.length" class="ma-4 text-no-wrap title">
         No courses.
       </v-row>
     </v-card-text>
@@ -129,7 +136,8 @@ export default {
   mixins: [Context, Utils],
   components: {Days, PageTitle, Spinner, ToggleOptOut},
   data: () => ({
-    courses: undefined,
+    eligibleCourses: [],
+    ineligibleCourses: [],
     headers: [
       {text: 'Course', value: 'label'},
       {text: 'Title', value: 'title'},
@@ -137,27 +145,28 @@ export default {
       {text: 'Room', value: 'room'},
       {text: 'Days', value: 'days'},
       {text: 'Time', value: 'time'},
-      {text: 'Opt out', value: 'hasOptedOut'}
     ],
     pageTitle: undefined,
     refreshingCourses: false,
   }),
   created() {
     this.$loading()
+    this.eligibleHeaders = this.headers.concat({text: 'Opt out', value: 'hasOptedOut'})
+    this.ineligibleHeaders = this.headers
     this.refreshCourses()
-    console.log(this.$currentUser)
-    this.pageTitle = `Your ${this.$config.currentTermName} Course${this.courses.length === 1 ? '' : 's'} Eligible for Capture`
+    this.pageTitle = `Your ${this.$config.currentTermName} Course${this.$currentUser.courses.length === 1 ? '' : 's'}`
     this.$ready(this.pageTitle)
   },
   methods: {
     refreshCourses() {
-      this.courses = this.$_.filter(this.$currentUser.courses, course => {
+      this.$_.each(this.$currentUser.courses, course => {
         course.courseCodes = this.getCourseCodes(course)
-        const isEligible = course.meetings.eligible.length
-        if (isEligible) {
-          course.meeting = this.getDisplayMeetings(course)[0]
+        course.displayMeetings = this.getDisplayMeetings(course)
+        if (course.meetings.eligible.length) {
+          this.eligibleCourses.push(course)
+        } else {
+          this.ineligibleCourses.push(course)
         }
-        return isEligible
       })
     },
     reloadCoursesTable() {
