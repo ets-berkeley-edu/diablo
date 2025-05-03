fd9d950639a766ece9d09e969d435e660c4f0b35
mohamalnadi <moalnadi@berkeley.edu>
2025-05-02 22:22:52 -0400

    Revert "diablo-1063 - initial PR for Vue 2 to Vue 3 upgrade"
    This reverts commit 3b8a3162031edac0897db6ea83ea847d49502bc7.

--- src-vue2/api/api-utils.ts
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-
-export default {
-  apiBaseUrl: () => Vue.prototype.$config.apiBaseUrl
-}
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import context from '@/store/modules/context'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// Vue.use(Vuex)
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
