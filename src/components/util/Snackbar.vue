8f4dd5358028f37eb9dd1f087c91f0bf8d20e3a6
Anne Lyttle <alyttle@berkeley.edu>
2025-05-28 13:21:24 -0700

    DIABLO-1100: prevents btn spinner from changing table height

--- src/components/util/Snackbar.vue
+++ src/components/util/Snackbar.vue
@@ -1,36 +1,35 @@
 <template>
-  <v-snackbar
-    v-model="snackbarShow"
-    :color="snackbar.color"
-    content-class="align-center"
-    location="top"
-    :timeout="snackbar.timeout"
-  >
-    <div class="d-flex align-center justify-space-between py-1">
-      <div
-        id="alert-text"
-        aria-live="polite"
-        class="px-4"
-        role="alert"
-      >
-        <div class="pb-4 text-h6">{{ snackbar.text }}</div>
-        <ContactUsPrompt
-          v-if="includeContactUsPrompt"
-          href-mailto-class="text-white"
-        />
+  <div id="snackbar-container" aria-live="assertive" role="alert">
+    <v-snackbar
+      v-model="snackbarShow"
+      attach="#snackbar-container"
+      :color="snackbar.color"
+      content-class="align-center"
+      location="top"
+      :timeout="snackbar.timeout"
+    >
+      <div class="d-flex align-center justify-space-between py-1">
+        <div id="alert-text" class="px-4">
+          <div class="text-h6">{{ snackbar.text }}</div>
+          <ContactUsPrompt
+            v-if="includeContactUsPrompt"
+            class="mb-4"
+            href-mailto-class="text-white"
+          />
+        </div>
+        <div>
+          <v-btn
+            id="btn-close-alert"
+            aria-label="Close alert"
+            variant="text"
+            @click="contextStore.snackbarClose"
+          >
+            Close
+          </v-btn>
+        </div>
       </div>
-      <div>
-        <v-btn
-          id="btn-close-alert"
-          aria-label="Close alert"
-          variant="text"
-          @click="contextStore.snackbarClose"
-        >
-          Close
-        </v-btn>
-      </div>
-    </div>
-  </v-snackbar>
+    </v-snackbar>
+  </div>
 </template>
 
 <script setup>
