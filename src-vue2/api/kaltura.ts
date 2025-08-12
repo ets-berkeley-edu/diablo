a0a8e8fdbe54a41f90fb5ed6d749006f2dfcf234
Pauline Kerschen <kerschen@berkeley.edu>
2025-08-12 13:21:37 -0700

    DIABLO-1182 Remove src-vue2 directory

--- src-vue2/api/kaltura.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-import utils from '@/api/api-utils'
-
-export function getKalturaCategory(courseSiteId) {
-  return axios.get(`${utils.apiBaseUrl()}/api/kaltura/category/canvas/${courseSiteId}`)
-}
